3b5073b330bfad1dceee8231929be8d717f6336d
Alexander V. Nikolaev <avn@avnik.info>
2024-05-21 21:23:38 +0300

    Add reflection

--- build.rs
+++ build.rs
@@ -1,8 +1,12 @@
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
     let proto = "api/admin/admin.proto";
 
-    tonic_build::compile_protos(proto).unwrap();
-
-    // prevent needing to rebuild if files (or deps) haven't changed
-    println!("cargo:rerun-if-changed={}", proto);
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    tonic_build::configure()
+        .file_descriptor_set_path(out_dir.join("admin_descriptor.bin"))
+        .compile(&["api/admin/admin.proto"], &["admin"])
+        .unwrap();
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,15 +1,28 @@
 use givc::server;
 use tonic::transport::Server;
 
+mod kludge {
+    pub const FILE_DESCRIPTOR_SET: &[u8] = tonic::include_file_descriptor_set!("admin_descriptor");
+}
+
 #[tokio::main]
 async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     let addr = "127.0.0.1:10000".parse().unwrap();
 
     let mut builder = Server::builder();
 
+    let reflect = tonic_reflection::server::Builder::configure()
+        .register_encoded_file_descriptor_set(kludge::FILE_DESCRIPTOR_SET)
+        .build()
+        .unwrap();
+
     let admin_service_svc = server::AdminServiceServer::new(server::AdminService::default());
 
-    builder.add_service(admin_service_svc).serve(addr).await?;
+    builder
+        .add_service(reflect)
+        .add_service(admin_service_svc)
+        .serve(addr)
+        .await?;
 
     Ok(())
 }
