ae959e1fc55fbf087f6d1b06236bd381a4d3272f
Alexander V. Nikolaev <avn@avnik.info>
2024-05-22 13:03:37 +0300

    Add basic CLI for args

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,13 +1,37 @@
+use clap::Parser;
 use givc::server;
+use std::net::SocketAddr;
 use tonic::transport::Server;
 
+#[derive(Debug, Parser)] // requires `derive` feature
+#[command(name = "givc-admin")]
+#[command(about = "A givc admin", long_about = None)]
+struct Cli {
+    #[arg(long, env = "ADDR", default_missing_value = "127.0.0.1")]
+    addr: String,
+    #[arg(long, env = "PORT", default_missing_value = "9000")]
+    port: u16,
+
+    #[arg(
+        long,
+        env = "SERVICES",
+        use_value_delimiter = true,
+        value_delimiter = ','
+    )]
+    services: Option<Vec<String>>,
+}
+
+// FIXME: should be in src/lib.rs: mod pb {}, but doesn't work
 mod kludge {
     pub const FILE_DESCRIPTOR_SET: &[u8] = tonic::include_file_descriptor_set!("admin_descriptor");
 }
 
 #[tokio::main]
 async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
-    let addr = "127.0.0.1:10000".parse().unwrap();
+    let cli = Cli::parse();
+    println!("CLI is {:#?}", cli);
+
+    let addr = SocketAddr::new(cli.addr.parse().unwrap(), cli.port);
 
     let mut builder = Server::builder();
 
