18ffd8382192eea53cda5e846898af227b330ab5
Alexander V. Nikolaev <avn@avnik.info>
2024-08-02 15:57:43 +0300

    Implement more testing functionality
    Signed-off-by: Alexander V. Nikolaev <avn@avnik.info>

--- src/bin/givc-cli.rs
+++ src/bin/givc-cli.rs
@@ -72,6 +72,28 @@ enum Commands {
         #[arg(long)]
         limit: Option<u32>,
     },
+    Test {
+        #[command(subcommand)]
+        test: Test,
+    },
+}
+
+#[derive(Debug, Subcommand)]
+enum Test {
+    Ensure { service: String },
+}
+
+async fn test_subcommands(test: Test, admin: AdminClient) -> anyhow::Result<()> {
+    match test {
+        Test::Ensure { service } => {
+            let reply = admin.query_list().await?;
+            if reply.iter().any(|r| r.name == service) {
+                Ok(())
+            } else {
+                anyhow::bail!("test failed '{service}' not registered")
+            }
+        }
+    }
 }
 
 #[tokio::main]
@@ -97,6 +119,7 @@ async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     let admin = AdminClient::new(cli.addr, cli.port, tls);
 
     match cli.command {
+        Commands::Test { test } => test_subcommands(test, admin).await?,
         Commands::Start { app } => admin.start(app).await?,
         Commands::Stop { app } => admin.stop(app).await?,
 
