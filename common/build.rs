cfc55ef0fca41b7db0c8229496e30c656d1c449d
Santtu Lakkala <santtu.lakkala@unikie.com>
2024-10-28 16:37:30 +0200

    Remove usage of deprecated method
    Signed-off-by: Santtu Lakkala <santtu.lakkala@unikie.com>

--- common/build.rs
+++ common/build.rs
@@ -5,16 +5,16 @@ fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     tonic_build::configure()
         .file_descriptor_set_path(out_dir.join("admin_descriptor.bin"))
-        .compile(&["api/admin/admin.proto"], &["admin"])
+        .compile_protos(&["api/admin/admin.proto"], &["admin"])
         .unwrap();
 
     tonic_build::configure()
         .file_descriptor_set_path(out_dir.join("locale_descriptor.bin"))
-        .compile(&["api/locale/locale.proto"], &["locale"])
+        .compile_protos(&["api/locale/locale.proto"], &["locale"])
         .unwrap();
 
     tonic_build::configure()
         .file_descriptor_set_path(out_dir.join("systemd_descriptor.bin"))
-        .compile(&["api/systemd/systemd.proto"], &["systemd"])
+        .compile_protos(&["api/systemd/systemd.proto"], &["systemd"])
         .unwrap();
 }
