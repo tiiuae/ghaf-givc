8cbcca2ac5552e79ec3a49cc7d771f731accb321
Alexander V. Nikolaev <avn@avnik.info>
2025-02-14 14:09:51 +0200

    WIP

--- nixos/tests/admin.nix
+++ nixos/tests/admin.nix
@@ -364,6 +364,10 @@ in
                   print(hostvm.succeed("${cli} ${cliArgs} set-timezone UTC"))
                   adminvm.wait_for_file("/etc/timezone.conf")
 
+              with subtest("OTA"):
+                  print(hostvm.succeed("${cli} ${cliArgs} list-generations"))
+                  assert False
+
               with subtest("get stats"):
                   print(hostvm.succeed("${cli} ${cliArgs} get-stats app-vm"))
 
