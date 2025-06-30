da9015bfa547cffda0543f7893701bca7382cec5
Vunny Sodhi <vunny.sodhi@unikie.com>
2025-06-30 09:25:16 +0300

    event: Support streaming of input devices
    Signed-off-by: Vunny Sodhi <vunny.sodhi@unikie.com>

--- modules/cmd/givc-agent/main.go
+++ modules/cmd/givc-agent/main.go
@@ -23,6 +23,7 @@ import (
 	givc_admin "givc/modules/api/admin"
 	givc_systemd "givc/modules/api/systemd"
 	givc_app "givc/modules/pkgs/applications"
+	givc_eventproxy "givc/modules/pkgs/eventproxy"
 	givc_grpc "givc/modules/pkgs/grpc"
 	givc_hwidmanager "givc/modules/pkgs/hwidmanager"
 	givc_localelistener "givc/modules/pkgs/localelistener"
@@ -284,6 +285,8 @@ func main() {
 		}
 	}
 
+	SetupEventService(agent, cfgAgent, tlsConfig)
+
 	// Register this instance
 	go func() {
 		// Wait for server to start
@@ -352,4 +355,75 @@ func main() {
 	if err != nil {
 		log.Fatalf("Grpc server failed: %v", err)
 	}
+}
+
+func SetupEventService(agent givc_types.TransportConfig, cfgAgent *givc_types.EndpointConfig, tlsConfig *tls.Config) {
+
+	var eventConfigs []givc_types.EventConfig
+	var err error
+	jsonEventproxyString, eventProxyOption := os.LookupEnv("EVENT_PROXY")
+	if eventProxyOption && jsonEventproxyString != "" {
+		err = json.Unmarshal([]byte(jsonEventproxyString), &eventConfigs)
+		if err != nil {
+			log.Fatalf("event: error unmarshalling JSON string: %v", err)
+		}
+	}
+
+	for _, eventConfig := range eventConfigs {
+
+		eventProxyServer, err := givc_eventproxy.NewEventProxyServer(eventConfig.Transport, eventConfig.Producer)
+		if err != nil {
+			log.Errorf("event: cannot create event proxy server: %v", err)
+		}
+
+		// Setup the server
+		if !eventConfig.Producer {
+			log.Infof("event: configuring event proxy server: %v", eventConfig)
+
+			go func(eventConfig givc_types.EventConfig) {
+
+				// Event proxy server config
+				cfgEventServer := &givc_types.EndpointConfig{
+					Transport: givc_types.TransportConfig{
+						Name:     eventConfig.Transport.Name,
+						Address:  eventConfig.Transport.Address,
+						Port:     eventConfig.Transport.Port,
+						Protocol: eventConfig.Transport.Protocol,
+					},
+					TlsConfig: tlsConfig,
+				}
+
+				var grpcProxyService []givc_types.GrpcServiceRegistration
+				grpcProxyService = append(grpcProxyService, eventProxyServer)
+				grpcServer, err := givc_grpc.NewServer(cfgEventServer, grpcProxyService)
+				if err != nil {
+					log.Errorf("event: cannot create grpc proxy server config: %v", err)
+				}
+				err = grpcServer.ListenAndServe(context.Background(), make(chan struct{}))
+				if err != nil {
+					log.Errorf("event: grpc socket proxy server failed: %v", err)
+				}
+
+			}(eventConfig)
+		} else {
+
+			go func(eventConfig givc_types.EventConfig) {
+
+				// Configure client endpoint
+				eventClient := &givc_types.EndpointConfig{
+					Transport: eventConfig.Transport,
+					TlsConfig: tlsConfig,
+				}
+
+				err = eventProxyServer.StreamEventsToRemote(context.Background(), eventClient)
+				for err != nil {
+					log.Errorf("event: client stream exited: %v", err)
+					time.Sleep(1 * time.Second)
+					err = eventProxyServer.StreamEventsToRemote(context.Background(), eventClient)
+				}
+
+			}(eventConfig)
+		}
+	}
+
 }
