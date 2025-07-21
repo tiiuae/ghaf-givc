e7ac564cba500a5f9fb172e2aae9a95dc4fed33c
Alexander V. Nikolaev <avn@avnik.info>
2025-07-21 12:01:11 +0300

    Make pin mandatory for query updates

--- crates/ota-update/src/query.rs
+++ crates/ota-update/src/query.rs
@@ -3,8 +3,11 @@ use anyhow::Context;
 
 /// # Errors
 /// Fails if unless fetch/parse update info from server
-pub async fn query_avaliable_updates(server_url: &str) -> anyhow::Result<Vec<UpdateInfo>> {
-    let url = format!("{server_url}/update");
+pub async fn query_available_updates(
+    server_url: &str,
+    pin_name: &str,
+) -> anyhow::Result<Vec<UpdateInfo>> {
+    let url = format!("{server_url}/update/{pin_name}");
     let info = reqwest::get(url.clone())
         .await
         .with_context(|| format!("Fail to fetch updates info! Url is {url}"))?
