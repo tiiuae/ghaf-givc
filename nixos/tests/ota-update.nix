7bb351620c0d77fbeb10b5c9b1134d0ddda62f54
Alexander V. Nikolaev <avn@avnik.info>
2025-07-21 11:29:00 +0300

    Missing public key
    Signed-off-by: Alexander V. Nikolaev <avn@avnik.info>

--- nixos/tests/ota-update.nix
+++ nixos/tests/ota-update.nix
@@ -76,6 +76,7 @@ let
         services.ota-update-server = {
           enable = true;
           allowedProfiles = [ "ghaf-updates" ];
+          publicKey = "test-updates.example.com:/muLakHVUJWxVRPIacpLJatGimj6S3OocBkwOan1VVc=%";
         };
         services.nginx = {
           enable = true;
