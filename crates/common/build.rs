ec9b5d818c10514669b18f0151dd7c888ee1f3ff
Santtu Lakkala <santtu.lakkala@unikie.com>
2025-09-11 17:10:59 +0300

    chore(deps): Update tonic stack to 0.14
    Signed-off-by: Santtu Lakkala <santtu.lakkala@unikie.com>

--- crates/common/build.rs
+++ crates/common/build.rs
@@ -4,13 +4,13 @@ use std::path::PathBuf;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     for pkg in ["admin", "exec", "locale", "systemd", "stats"] {
-        tonic_build::configure()
+        tonic_prost_build::configure()
             .file_descriptor_set_path(out_dir.join(format!("{pkg}_descriptor.bin")))
             .type_attribute(
                 ".admin.Generation",
                 "#[derive(Deserialize, Serialize)] #[serde(rename_all = \"camelCase\")]",
             )
-            .compile_protos(&[&format!("api/{pkg}/{pkg}.proto")], &["api"])?;
+            .compile_protos(&[format!("api/{pkg}/{pkg}.proto").as_str()], &["api"])?;
     }
 
     Ok(())
