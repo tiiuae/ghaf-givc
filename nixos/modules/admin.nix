5119669b913648ff22bad0b9a141625ff4a009c4
Ganga Ram <Ganga.Ram@tii.ae>
2025-11-12 17:02:09 +0400

    open-policy-agent module
    Signed-off-by: Ganga Ram <Ganga.Ram@tii.ae>

--- nixos/modules/admin.nix
+++ nixos/modules/admin.nix
@@ -9,6 +9,8 @@
 }:
 let
   cfg = config.givc.admin;
+  opacfg = cfg.open-policy-agent;
+  updatercfg = cfg.open-policy-agent.policy.updater;
   inherit (self.packages.${pkgs.stdenv.hostPlatform.system}) givc-admin;
   inherit (lib)
     mkOption
@@ -29,6 +31,12 @@ let
   tcpAddresses = lib.filter (addr: addr.protocol == "tcp") cfg.addresses;
   unixAddresses = lib.filter (addr: addr.protocol == "unix") cfg.addresses;
   vsockAddresses = lib.filter (addr: addr.protocol == "vsock") cfg.addresses;
+  opaServerPort = 8181;
+  policyName = "ghaf-policy";
+  policy_bundle = pkgs.fetchurl {
+    url = "${opacfg.policy.url}/${opacfg.policy.resource}";
+    hash = "${opacfg.policy.sha256}";
+  };
 in
 {
   options.givc.admin = {
@@ -120,6 +128,68 @@ in
         > It is recommended to use a global TLS flag to avoid inconsistent configurations that will result in connection errors.
       '';
     };
+
+    open-policy-agent = {
+      enable = mkEnableOption "Start open policy agent service.";
+      policy = {
+        url = lib.mkOption {
+          type = lib.types.str;
+          example = "https://github.com/gngram/policy-store/archive/refs/heads";
+          description = "Base URL for fetching the OPA policy archive.";
+        };
+
+        resource = lib.mkOption {
+          type = lib.types.str;
+          default = "main";
+          description = "Archive resource path (e.g., main.tar.gz) appended to the base URL.";
+        };
+
+        sha256 = lib.mkOption {
+          type = lib.types.str;
+          description = "sha256 of the resource archive.";
+        };
+
+        updater = {
+          enable = mkEnableOption "Download latest policy from the provided policy store";
+          url = lib.mkOption {
+            type = lib.types.str;
+            example = "https://github.com/gngram/policy-store/archive/refs/heads";
+            description = "Base URL for fetching the OPA policy archive. Defaults to policy.url";
+            default = opacfg.policy.url;
+          };
+
+          resource = lib.mkOption {
+            type = lib.types.str;
+            description = "Archive resource path (e.g., main) appended to the base URL.";
+            default = opacfg.policy.resource;
+          };
+
+          minDelay = lib.mkOption {
+            type = lib.types.int;
+            default = 10;
+            description = "Minimum polling delay (seconds). Must be > 0 and <= maxDelay.";
+          };
+
+          maxDelay = lib.mkOption {
+            type = lib.types.int;
+            default = 30;
+            description = "Maximum polling delay (seconds). Must be >= minDelay.";
+          };
+
+          roots = lib.mkOption {
+            type = lib.types.listOf lib.types.str;
+            default = [ "policy-store-main" ];
+            description = "List of root paths OPA should activate from the bundle.";
+          };
+
+          token = lib.mkOption {
+            type = lib.types.nullOr lib.types.str;
+            default = null;
+            description = "Access token for policy repository.";
+          };
+        };
+      };
+    };
   };
 
   config = mkIf cfg.enable {
@@ -129,7 +199,97 @@ in
           !(cfg.tls.enable && (cfg.tls.caCertPath == "" || cfg.tls.certPath == "" || cfg.tls.keyPath == ""));
         message = "The TLS option requires paths' to CA certificate, service certificate, and service key.";
       }
+
+      {
+        assertion = !(opacfg.enable && updatercfg.enable && updatercfg.minDelay <= 0);
+        message = "admin.givc.open-policy-agent.policy.liveUpdate.minDelay must be > 0.";
+      }
+
+      {
+        assertion = !(opacfg.enable && updatercfg.enable && updatercfg.minDelay >= updatercfg.maxDelay);
+        message = "admin.givc.open-policy-agent.policy.liveUpdate.maxDelay must be > minDelay.";
+      }
+
+      {
+        assertion = !(opacfg.enable && updatercfg.enable && updatercfg.roots == [ ]);
+        message = "admin.givc.open-policy-agent.policy.liveUpdate.roots must not be empty.";
+      }
     ];
+
+    environment.etc = {
+      "open-policy-agent/access-token" =
+        mkIf (opacfg.enable && updatercfg.enable && updatercfg.token != null)
+          {
+            text = updatercfg.token;
+            mode = "0400";
+            user = "opa";
+            group = "opa";
+          };
+
+      "open-policy-agent/bundle.tar.gz" = mkIf opacfg.enable {
+        source = policy_bundle;
+      };
+
+      "open-policy-agent/config.yaml" = mkIf (opacfg.enable && updatercfg.enable) {
+        text = ''
+          persistence_directory: "/run/open-policy-agent"
+          services:
+            - name: ${policyName}
+              url: ${updatercfg.url}
+              credentials:
+                bearer:
+                  token_path: "/etc/open-policy-agent/access-token"
+
+          bundles:
+            ${policyName}:
+              service: ${policyName}
+              resource: ${updatercfg.resource}
+              roots: [${lib.concatMapStringsSep " " (r: "\"${r}\"") updatercfg.roots}]
+              persist: true
+              polling:
+                min_delay_seconds: ${toString updatercfg.minDelay}
+                max_delay_seconds: ${toString updatercfg.maxDelay}
+        '';
+      };
+    };
+
+    systemd.services.open-policy-agent = mkIf opacfg.enable {
+      description = "Open Policy Agent";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+      serviceConfig = {
+        user = "opa";
+        group = "opa";
+        ExecStartPre =
+          let
+            preStartScript = pkgs.writeScript "opa-prestart" ''
+              #!${pkgs.bash}/bin/bash
+              policyDir=/run/open-policy-agent/bundles/${policyName}/
+
+              if ! test -d "$policyDir"; then
+                install -d -m 0755 -o root -g root "$policyDir"
+                cp /etc/open-policy-agent/bundle.tar.gz $policyDir/
+              elif ! test -f "$policyDir/bundle.tar.gz"; then
+                cp /etc/open-policy-agent/bundle.tar.gz $policyDir/
+              fi
+            '';
+          in
+          "!${preStartScript}";
+        ExecStart = ''
+          ${pkgs.open-policy-agent}/bin/opa run \
+            --server \
+            --addr localhost:${toString opaServerPort} \
+            ${lib.optionalString updatercfg.enable "--config-file /etc/open-policy-agent/config.yaml"} \
+            ${
+              lib.optionalString (
+                !updatercfg.enable
+              ) "--bundle /run/open-policy-agent/bundles/${policyName}/bundle.tar.gz"
+            } \
+        '';
+
+        Restart = "always";
+      };
+    };
 
     systemd.services.givc-admin =
       let
@@ -168,6 +328,9 @@ in
           "GIVC_LOG" = "givc=debug,info";
         };
       };
-    networking.firewall.allowedTCPPorts = unique (map (addr: strings.toInt addr.port) tcpAddresses);
+
+    networking.firewall.allowedTCPPorts = unique (
+      (map (addr: strings.toInt addr.port) tcpAddresses) ++ lib.optional opacfg.enable opaServerPort
+    );
   };
 }
