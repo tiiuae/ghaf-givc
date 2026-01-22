06ef183baa7d522a6ffe671a1f6ac7752b15fedb
Santtu Lakkala <santtu.lakkala@unikie.com>
2026-01-22 16:42:22 +0200

    feat(exec): Implement streaming client method
    Implement streaming command method in the exec client, was already
    supported by the protocol.
    
    Further allow signaling stdin closure for running programs that process
    all input.

--- crates/client/src/exec.rs
+++ crates/client/src/exec.rs
@@ -1,5 +1,8 @@
 use anyhow::Context;
+use async_stream::{stream, try_stream};
 use std::future::Future;
+use std::pin::Pin;
+use tokio_stream::{Stream, StreamExt};
 use tonic::Request;
 use tonic::transport::Channel;
 use tracing::{debug, warn};
@@ -18,6 +21,11 @@ pub struct ExecClient {
     client: Client,
 }
 
+pub enum CommandOutput {
+    Stdout(Vec<u8>),
+    Stderr(Vec<u8>),
+}
+
 impl ExecClient {
     /// Connects to the gRPC server at the specified address
     /// # Errors
@@ -28,11 +36,95 @@ impl ExecClient {
         Ok(Self { client })
     }
 
+    /// Starts a command with bidirectional streaming
+    /// # Errors
+    /// Raise error if unable to connect
+    pub async fn start_command_stream(
+        &mut self,
+        command: String,
+        arguments: Vec<String>,
+        working_directory: Option<String>,
+        env_vars: Option<std::collections::HashMap<String, String>>,
+        mut stdin: impl Stream<Item = Vec<u8>> + std::marker::Unpin + Send + Sync + 'static,
+        role: Option<String>,
+    ) -> anyhow::Result<Pin<Box<dyn Stream<Item = anyhow::Result<CommandOutput>> + Send + Sync>>>
+    {
+        let start_command = StartCommand {
+            command,
+            arguments,
+            working_directory,
+            env_vars: env_vars.unwrap_or_default(),
+            stdin: Some(vec![]),
+            role,
+        };
+
+        // Create a streaming request
+        let request_stream = stream! {
+            // Send the StartCommand
+            yield CommandRequest {
+                command: Some(Command::Start(start_command)),
+            };
+
+            while let Some(input) = stdin.next().await {
+                yield CommandRequest {
+                    command: Some(Command::Stdin(CommandIo { payload: input, eof: false }))
+                };
+            }
+
+            yield CommandRequest { command: Some(Command::Stdin(CommandIo { payload: vec![], eof: true })) };
+        };
+
+        // Open the request stream and capture responses
+        let mut response = self
+            .client
+            .run_command(Request::new(request_stream))
+            .await?
+            .into_inner();
+
+        let response_stream = try_stream! {
+            while let Some(response) = response.message().await? {
+                match response.event {
+                    Some(Event::Stdout(CommandIO { payload, .. })) => {
+                        debug!(
+                            "Event::Stdout {len} bytes: {out}",
+                            len = payload.len(),
+                            out = String::from_utf8_lossy(&payload)
+                        );
+                        yield CommandOutput::Stdout(payload);
+                    }
+                    Some(Event::Stderr(CommandIO { payload, .. })) => {
+                        debug!(
+                            "Event::Stderr {len} bytes: {out}",
+                            len = payload.len(),
+                            out = String::from_utf8_lossy(&payload)
+                        );
+                        yield CommandOutput::Stderr(payload);
+                    }
+                    Some(Event::Started(started)) => {
+                        debug!("Process started with PID: {}", started.pid);
+                    }
+                    Some(Event::Finished(finished)) => {
+                        debug!("Process finished with exit code: {}", finished.return_code);
+                    }
+                    None => {
+                        warn!("Received empty response");
+                    }
+                }
+            }
+
+            check_trailers(response)
+                .await
+                .context("While check trailers in exec.rs")?;
+        };
+
+        Ok(Box::pin(response_stream))
+    }
+
     /// Starts a subprocess on the server with the given command and arguments
     /// # Errors
     /// Raise error if program unable to execute, or on gRPC IO errors
     #[allow(clippy::too_many_arguments)]
-    pub async fn start_command<SO, SE, SOA, SEA>(
+    pub async fn start_command<SOA, SEA>(
         &mut self,
         command: String,
         arguments: Vec<String>,
@@ -40,15 +132,14 @@ impl ExecClient {
         env_vars: Option<std::collections::HashMap<String, String>>,
         stdin: Option<Vec<u8>>,
         role: Option<String>,
-        mut stdout_fn: SO,
-        mut stderr_fn: SE,
+        mut stdout_fn: impl FnMut(Vec<u8>, bool) -> SOA,
+        mut stderr_fn: impl FnMut(Vec<u8>, bool) -> SEA,
     ) -> anyhow::Result<i32>
     where
-        SO: FnMut(Vec<u8>) -> SOA,
-        SE: FnMut(Vec<u8>) -> SEA,
         SOA: Future<Output = ()>,
         SEA: Future<Output = ()>,
     {
+        let has_stdin = stdin.is_some();
         let start_command = StartCommand {
             command,
             arguments,
@@ -59,12 +150,17 @@ impl ExecClient {
         };
 
         // Create a streaming request
-        let request_stream = tokio_stream::once(
+        let request_stream = stream! {
             // Send the StartCommand
-            CommandRequest {
+            yield CommandRequest {
                 command: Some(Command::Start(start_command)),
-            },
-        );
+            };
+            if has_stdin {
+                yield CommandRequest {
+                    command: Some(Command::Stdin(CommandIO { payload: vec![], eof: true })),
+                };
+            }
+        };
 
         // Open the request stream and capture responses
         let mut response = self
@@ -80,21 +176,21 @@ impl ExecClient {
         // Process the server's responses
         while let Some(response) = response.message().await? {
             match response.event {
-                Some(Event::Stdout(CommandIO { payload })) => {
+                Some(Event::Stdout(CommandIO { payload, eof })) => {
                     debug!(
                         "Event::Stdout {} bytes: {out}",
                         payload.len(),
                         out = String::from_utf8_lossy(&payload)
                     );
-                    stdout_fn(payload).await;
+                    stdout_fn(payload, eof).await;
                 }
-                Some(Event::Stderr(CommandIO { payload })) => {
+                Some(Event::Stderr(CommandIO { payload, eof })) => {
                     debug!(
                         "Event::Stderr {} bytes: {out}",
                         payload.len(),
                         out = String::from_utf8_lossy(&payload)
                     );
-                    stderr_fn(payload).await;
+                    stderr_fn(payload, eof).await;
                 }
                 Some(Event::Started(started)) => {
                     debug!("Process started with PID: {}", started.pid);
@@ -140,11 +236,11 @@ impl ExecClient {
                 env_vars,
                 stdin,
                 role,
-                |payload| {
+                |payload, _eof| {
                     stdout_buffer.extend(&payload);
                     std::future::ready(())
                 },
-                |payload| {
+                |payload, _eof| {
                     stderr_buffer.extend(&payload);
                     std::future::ready(())
                 },
